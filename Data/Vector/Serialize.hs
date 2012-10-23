6f8e3abfdcaf4c3d9ee78e961f414c969d79b0b7
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-10-23 16:49:36 +0400

    Add vector encoders and decoders with custom get and put.
    They are useful when instance is not suitable for some reasons
    or when there is no instance at all.

--- Data/Vector/Serialize.hs
+++ Data/Vector/Serialize.hs
@@ -9,16 +9,18 @@
 -- they must be declared on an individual basis for each type the
 -- vectors may contain. The 'genericGet' and 'genericPut' functions
 -- should still work for these vectors without declaring instances.
--- 
+--
 -- The serialized format is an 'Int64' representing the
 -- length of the 'Vector', followed by the serialized contents of each
 -- element.
--- 
+--
 -- Note that the instances specialized for 'Data.Vector.Storable' are
 -- much more performant for storable vectors.
 module Data.Vector.Serialize (
     genericGetVector
   , genericPutVector
+  , genericGetVectorWith
+  , genericPutVectorWith
   ) where
 
 import Control.Monad
@@ -29,21 +31,36 @@ import qualified Data.Vector as V
 import qualified Data.Vector.Primitive as VP
 import qualified Data.Vector.Generic as VG
 
--- | Read a 'Data.Vector.Generic.Vector'.
-genericGetVector :: (Serialize a, VG.Vector v a) => Get (v a)
-{-# INLINE genericGetVector #-}
-genericGetVector = do 
+
+-- | Read a 'Data.Vector.Generic.Vector' using custom decoder for
+--   vector's elements.
+genericGetVectorWith :: (VG.Vector v a) => Get a -> Get (v a)
+{-# INLINE genericGetVectorWith #-}
+genericGetVectorWith getter = do
   len64 <- (get :: Get Int64)
   when (len64 > fromIntegral (maxBound :: Int)) $
     fail "Host can't deserialize a Vector longer than (maxBound :: Int)"
-  VG.replicateM (fromIntegral len64) get
+  VG.replicateM (fromIntegral len64) getter
+
+-- | Write a 'Data.Vector.Generic.Vector' using custom putter for
+--   vector's elements.
+genericPutVectorWith :: (VG.Vector v a) => Putter a -> Putter (v a)
+{-# INLINE genericPutVectorWith #-}
+genericPutVectorWith putter v = do
+  put $ ((fromIntegral $ VG.length v) :: Int64)
+  VG.mapM_ putter v
+
 
 -- | Write a 'Data.Vector.Generic.Vector'.
 genericPutVector :: (Serialize a, VG.Vector v a) => Putter (v a)
 {-# INLINE genericPutVector #-}
-genericPutVector v = do
-  put $ ((fromIntegral $ VG.length v) :: Int64)
-  VG.mapM_ put v
+genericPutVector = genericPutVectorWith put
+
+-- | Read a 'Data.Vector.Generic.Vector'.
+genericGetVector :: (Serialize a, VG.Vector v a) => Get (v a)
+{-# INLINE genericGetVector #-}
+genericGetVector = genericGetVectorWith get
+
 
 instance (Serialize a) => Serialize (V.Vector a) where
   get = genericGetVector ; put = genericPutVector
